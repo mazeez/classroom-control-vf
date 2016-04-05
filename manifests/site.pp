a489b631cb8fd79c902bafdec9ef7d40e4094ea2
mazeez <sharjeel.azeez@gmail.com>
2016-04-05 10:04:00 -0400

    Managing /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,5 +42,12 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  file { '/etc/motd':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0644',
+  content => "Hey, Training Day - Day 2",
+}
   notify { "Hello, my name is ${::hostname}": }
 }
