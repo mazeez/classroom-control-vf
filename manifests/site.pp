08a9a4d0345a4db888f9b2774c4ac8343cc4a3d5
root <root@mazeez.puppetlabs.vm>
2016-04-06 14:10:20 +0000

    Removed default classification

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include users
-  include skeleton
+ # include users
+ # include skeleton
 }
